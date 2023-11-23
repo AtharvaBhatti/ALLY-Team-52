cbab5841df50517890d1e8e0cc53f7188bccc539
Shubham <shubhamy@iitbhilai.ac.in>
2023-11-23 13:54:37 +0530

    register hackathon method modified

--- Back-end/Ally/main/hackathon.py
+++ Back-end/Ally/main/hackathon.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response # any python data we pass into it will be rendered out as json data by response
 from rest_framework.decorators import api_view
 from .models import HackathonRegistration,Hackathon, UserDetails
@@ -26,8 +27,14 @@ def getHackReg(request):
 
 @api_view(['POST'])
 def registerHackathon(request):
+    hackathon_id = request.data.get('hackathonID')
+    try:
+        hackathon_instance = Hackathon.objects.get(pk=hackathon_id)
+
+    except Hackathon.DoesNotExist:
+        return Response({"error": "Hackathon not found"}, status=status.HTTP_404_NOT_FOUND)
     serializer = HackathonRegistrationSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        serializer.save(hackathonID=hackathon_instance)
 
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
