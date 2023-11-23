2f29582ecdf078399eb4e2707056f26667ebb343
vinayak180203 <vinayakk@iitbhilai.ac.in>
2023-11-23 16:08:58 +0530

    Added sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage.jsx";
 import Navbar from "./Components/Navbar.jsx";
+import SidebarPage from "./Components/Sidebar.jsx"
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <SidebarPage/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
