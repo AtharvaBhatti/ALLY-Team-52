333bf08469c46b37f58faedfbaa32b5a333982e5
Shubham <shubhamy@iitbhilai.ac.in>
2023-11-23 11:42:46 +0530

    Hackathon models added

--- Back-end/Ally/main/models.py
+++ Back-end/Ally/main/models.py
@@ -46,3 +46,28 @@ class Post(models.Model):
     likesCount = models.IntegerField(default=0)
     commentsCount = models.IntegerField(default=0)
     tags = models.ManyToManyField(Tag)
+
+class Hackathon(models.Model):
+    name = models.CharField(max_length=100)
+    oneLiner=models.CharField(max_length=100)
+    description=models.TextField()
+    conductedBy=models.CharField(max_length=100)
+    institute = models.CharField(max_length=100)
+    openToALL = models.CharField(max_length=100)
+    postedOn = models.DateTimeField(auto_now_add=True)
+    startDate = models.DateTimeField(auto_now_add=True)
+    endDate = models.DateTimeField(auto_now_add=True)
+    cost = models.IntegerField()
+    registeredCount = models.IntegerField()
+    winner = models.CharField(max_length=100)
+    runnerUp = models.CharField(max_length=100)
+    metaData = models.CharField(max_length=100)
+    otherWinners = models.CharField(max_length=100)
+
+class HackathonRegistration(models.Model):
+    hackathonID = models.ForeignKey(Hackathon, on_delete=models.CASCADE)
+    teamLeader = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    teamMembers = models.TextField()
+    submission = models.CharField(max_length=100)
+    submissionTime = models.DateTimeField(auto_now_add=True)
+    registeredTime = models.DateTimeField(auto_now_add=True)
