b243e7d16c326b22eb61643401dbeb82f0434464
aishikanandi <aishikanandi@gmail.com>
2023-11-23 19:26:32 +0530

    Forums Basic Template Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage.jsx";
 import Navbar from "./Components/Navbar.jsx";
-import SidebarPage from "./Components/Sidebar.jsx"
+import SinglePost from "./Pages/Forum/SinglePost.jsx" 
+import Forum from "./Pages/Forum/ForumPage.jsx";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/uni1" element={<Homepage />}></Route>
+          <Route path="/uni1/forum" element={<Forum />}></Route>
+          <Route path="/uni1/forum/id" element={<SinglePost/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
